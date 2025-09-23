bd48d3ac08754b0259923888cf99b25a6c0aee9b
Alibi Yermekov <alibiyermekov@Alibis-MacBook-Pro.local>
2025-09-23 18:15:13 +0200

    push: new files

--- pair_plot.py
+++ pair_plot.py
@@ -4,12 +4,8 @@ from core.visualizations import pair_plot
 if __name__ == "__main__":
     try:
         dataset = validate("datasets/dataset_train.csv")
-        hi = ["Arithmancy",
-              "Astronomy",
-              "Herbology",
+        hi = [
               "Defense Against the Dark Arts",
-              "Divination",
-              "Muggle Studies",
               "Ancient Runes"]
         pair_plot(dataset, features=hi)
     except Exception as e:
