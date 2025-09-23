2b3cbc41433b35cda9d0531c1bcb28d046392157
Alibi Yermekov <alibiyermekov@Alibis-MacBook-Pro.local>
2025-09-23 16:52:25 +0200

    push: adding which features to show

--- pair_plot.py
+++ pair_plot.py
@@ -4,7 +4,13 @@ from core.visualizations import pair_plot
 if __name__ == "__main__":
     try:
         dataset = validate("datasets/dataset_train.csv")
-        specific_features = ["Arithmancy", "Herbology", "Transfiguration", "Potions"]
-        pair_plot(dataset, features=specific_features)
+        hi = ["Arithmancy",
+              "Astronomy",
+              "Herbology",
+              "Defense Against the Dark Arts",
+              "Divination",
+              "Muggle Studies",
+              "Ancient Runes"]
+        pair_plot(dataset, features=hi)
     except Exception as e:
         print(f"{type(e).__name__}: {e}")
