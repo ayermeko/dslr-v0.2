a5202a9c9e34cd943941ed6b4867ac7e1ef6b622
Alibi Yermekov <alibiyermekov@Alibis-MacBook-Pro.local>
2025-09-26 14:46:45 +0200

    push: changes

--- pair_plot.py
+++ pair_plot.py
@@ -1,14 +1,18 @@
-from core.operations import validate
+from core.operations import validate, sortout_col
 from core.visualizations import pair_plot
 
 if __name__ == "__main__":
     try:
         dataset = validate("datasets/dataset_train.csv")
-        hi = ['Defense Against the Dark Arts', 
-              'Charms',
-              'Herbology',
-              'Divination',
-              'Muggle Studies']
-        pair_plot(dataset, features=hi)
+        # hi = ['Arithmancy', 'Astronomy']
+        _, hi = sortout_col(dataset)
+        truing = []
+        for cal_name in hi:
+            if cal_name == 'Ancient Runes' or cal_name == 'Defense Against the Dark Arts':
+                truing.append(cal_name)
+            continue
+        print(truing)
+        print(hi)
+        # pair_plot(dataset, features=hi)
     except Exception as e:
         print(f"{type(e).__name__}: {e}")
