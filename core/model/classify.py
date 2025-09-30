7394a965664a9a3c03a3d88264b2e619ffde43bf
Alibi Yermekov <alibiyermekov@Alibis-MacBook-Pro.local>
2025-09-30 12:24:34 +0200

    push: created classifier class

---
+++ core/model/classify.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class LogisticRegression:
+    def normalize(self, X):
+        """Stadardize features with (mean=0, std=1)"""
+        pass
+
+    def fit():
+        pass
+
+    def save_model():
+        pass
+
--- logreg_train.py
+++ logreg_train.py
@@ -1,6 +1,7 @@
 import sys
-from core.operations import validate
 from core.model.preprocessing import clear_data, split_randomize
+from core.model.classify import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 def main():
     try:
@@ -10,6 +11,18 @@ def main():
         # loading selected features
         X_train, X_test, y_train, y_test = split_randomize(X, y)
 
+        model = LogisticRegression()
+        X_train_norm = model.normalize(X_train)
+        X_test_norm = model.normalize(X_test)
+
+        model.fit(X_train_norm, y_train)
+
+        y_pred = model.predict(X_train_norm)
+
+        print(f'Misclasified samples: {sum(y_test != y_pred)}')
+        print(f'Accuracy: {accuracy_score(y_test, y_pred):.2f}')
+
+        model.save_model()
 
     except Exception as e:
         print(f"{type(e).__name__}: {e}")
